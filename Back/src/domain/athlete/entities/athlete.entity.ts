44c485205ab88f45e5eb8f05c0985d484e052983
Hauagge <gsantos@viptech.com.br>
2026-03-13 13:40:12 -0300

    refactor(AthleteService, AthleteEntity): remove unused dependencies and streamline athlete creation process

--- Back/src/domain/athlete/entities/athlete.entity.ts
+++ Back/src/domain/athlete/entities/athlete.entity.ts
@@ -1,6 +1,3 @@
-import { Belt } from 'src/domain/belt/entities/belt.entity';
-import { Bracket } from 'src/domain/bracket/entities/bracket.entity';
-import { Category } from 'src/domain/category/entities/categoory.entitty';
 import {
   Entity,
   PrimaryGeneratedColumn,
@@ -26,9 +23,6 @@ export class Athlete {
   @Column({ name: 'belt_id' })
   beltId: number;
 
-  @ManyToOne(() => Belt, (belt) => belt.athletes)
-  belt: Belt;
-
   @Column({ name: 'category_id' })
   categoryId: number;
 
@@ -41,15 +35,9 @@ export class Athlete {
   @Column({ nullable: true })
   fightBracketId: number;
 
-  @ManyToOne(() => Bracket, (bracket) => bracket.athletes)
-  bracket: Bracket;
-
   @Column()
   tutor: string;
 
   @Column()
   subscriptionNumber: string;
-
-  @ManyToOne(() => Category, (category) => category.athletes)
-  category: Category;
 }
