7be099dcdd0c92fd7dbefae043e31f97927d0c76
Hauagge <gsantos@viptech.com.br>
2026-03-16 18:32:10 -0300

    feat: add CompetitionModule to AppModule and enable CORS in main.ts

--- Back/src/app.module.ts
+++ Back/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './domain/auth/auth.module';
+import { CompetitionModule } from './domain/competition/competition.module';
 
 @Module({
-  imports: [DatabaseModule, AuthModule],
+  imports: [DatabaseModule, AuthModule, CompetitionModule],
   controllers: [AppController],
   providers: [AppService],
 })
