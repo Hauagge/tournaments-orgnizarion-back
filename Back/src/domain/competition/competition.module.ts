afb1831515fa29cb7f2004c2cc75a40f03ef0476
Hauagge <gsantos@viptech.com.br>
2026-03-16 18:32:04 -0300

    feat: create CompetitionModule to manage competition-related use cases and dependencies

---
+++ Back/src/domain/competition/competition.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CreateCompetitionUseCase } from './application/use-cases/create-competition.use-case';
+import { GetCompetitionUseCase } from './application/use-cases/get-competition.use-case';
+import { ListCompetitionsUseCase } from './application/use-cases/list-competitions.use-case';
+import { UpdateCompetitionSettingsUseCase } from './application/use-cases/update-competition-settings.use-case';
+import { CompetitionController } from './infra/http/competition.controller';
+import { CompetitionRepository } from './infra/persistence/competition.repository';
+import { CompetitionTypeOrmEntity } from './infra/persistence/entities/competition.entity';
+import { ICompetitionRepository } from './repository/ICompetitionRepository.repository';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([CompetitionTypeOrmEntity])],
+  controllers: [CompetitionController],
+  providers: [
+    CreateCompetitionUseCase,
+    UpdateCompetitionSettingsUseCase,
+    GetCompetitionUseCase,
+    ListCompetitionsUseCase,
+    {
+      provide: ICompetitionRepository,
+      useClass: CompetitionRepository,
+    },
+  ],
+  exports: [
+    CreateCompetitionUseCase,
+    UpdateCompetitionSettingsUseCase,
+    GetCompetitionUseCase,
+    ListCompetitionsUseCase,
+    ICompetitionRepository,
+  ],
+})
+export class CompetitionModule {}
