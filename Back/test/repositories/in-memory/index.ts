c7ee00e06b6899f8b1ebca6b2dc87e0cf3326df9
Hauagge <gsantos@viptech.com.br>
2026-03-16 18:33:14 -0300

    feat: include in-memory-competition.repository in index export

--- Back/test/repositories/in-memory/index.ts
+++ Back/test/repositories/in-memory/index.ts
@@ -1,5 +1,5 @@
 export * from './in-memory-athlete.repository';
 export * from './in-memory-auth.repository';
 export * from './in-memory-championship.repository';
+export * from './in-memory-competition.repository';
 export * from './in-memory-fight.repository';
-
