2e3180cb35baa4380e759000f6e4fc057b32d701
Hauagge <gsantos@viptech.com.br>
2026-03-16 18:32:56 -0300

    feat: add factory function for creating Competition instances

---
+++ Back/test/factories/competition.factory.ts
@@ -0,0 +1,17 @@
+import { Competition } from '../../src/domain/competition/domain/entities/competition.entity';
+import { CompetitionMode } from '../../src/domain/competition/domain/value-objects/competition-mode.enum';
+
+export function makeCompetition(
+  overrides: Partial<ReturnType<Competition['toJSON']>> = {},
+): Competition {
+  return Competition.restore({
+    id: 1,
+    name: 'Competition Test',
+    mode: CompetitionMode.TEAM,
+    fightDurationSeconds: 300,
+    weighInMarginGrams: 500,
+    ageSplitYears: 2,
+    createdAt: new Date('2026-01-10T00:00:00.000Z'),
+    ...overrides,
+  });
+}
