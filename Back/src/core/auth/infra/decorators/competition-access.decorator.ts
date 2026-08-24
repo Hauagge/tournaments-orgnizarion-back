ec11d002595b44352b5e38c0ad8a27ce3f17d27c
Hauagge <gsantos@viptech.com.br>
2026-08-24 08:39:51 -0300

    feat: add CompetitionAccess decorator for competition access control

---
+++ Back/src/core/auth/infra/decorators/competition-access.decorator.ts
@@ -0,0 +1,12 @@
+import { SetMetadata } from '@nestjs/common';
+
+export type CompetitionAccessResolver =
+  | { type: 'competition'; param: string }
+  | { type: 'category'; param: string }
+  | { type: 'keyGroup'; param: string }
+  | { type: 'fight'; param: string };
+
+export const COMPETITION_ACCESS_KEY = 'competitionAccessResolver';
+
+export const CompetitionAccess = (resolver: CompetitionAccessResolver) =>
+  SetMetadata(COMPETITION_ACCESS_KEY, resolver);
